4446c6a525298673a1cc8c7a1709bc30035ccbe8
Echoda David <davidechoda123@gmail.com>
2024-01-07 10:12:48 +0100

    added auth guard flow diagram

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getOk(): string {
-    throw new InternalServerErrorException("something went wrong");
+    return "Server works fine"
   }
 }
