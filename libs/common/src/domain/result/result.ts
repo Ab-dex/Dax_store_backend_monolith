811d85f1bf08902e62fdd8b05405d04eb02ae613
Echoda David <davidechoda123@gmail.com>
2024-01-10 00:49:16 +0100

    make response symmetrical across every instance

--- libs/common/src/domain/result/result.ts
+++ libs/common/src/domain/result/result.ts
@@ -1,4 +1,5 @@
-import { HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 export class Result<T> {
   isSuccess: boolean;
   private data?: any;
@@ -7,7 +8,7 @@ export class Result<T> {
   errorCode: HttpStatus;
   constructor(isSuccess: boolean, data?: any, message?: string, errorCode?: HttpStatus) {
     this.data = isSuccess ? data : undefined;
-    this.error = !isSuccess &&  {statusCode: errorCode, details: message}
+    this.error = !isSuccess ? {statusCode: errorCode, details: message} : undefined
     this.isSuccess = isSuccess;
     this.message = isSuccess ? message : undefined;
   }
